397be863b2cc4dc489bd4b61af5afc0e92f79777
Vikas <vikas1000dhiman@outlook.com>
2024-09-04 08:24:48 +0530

    attitional .env variables

--- client/src/utils/CustomFetch.js
+++ client/src/utils/CustomFetch.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://craftoras.onrender.com/api/v1'
+    : 'http://localhost:5000/api/v1'
+
 const CustomFetch = axios.create({
-  baseURL: 'https://craftoras.onrender.com/api/v1',
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
